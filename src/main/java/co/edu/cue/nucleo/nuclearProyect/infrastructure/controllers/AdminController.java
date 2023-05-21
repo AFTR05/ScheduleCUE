fcfe7af5429ad1582dbc589a16ba993009f7875d
Andres <atoro1025@cue.edu.co>
2023-05-21 09:55:25 -0500

    Base data connected

--- src/main/java/co/edu/cue/nucleo/nuclearProyect/infrastructure/controllers/AdminController.java
+++ src/main/java/co/edu/cue/nucleo/nuclearProyect/infrastructure/controllers/AdminController.java
@@ -2,15 +2,16 @@ package co.edu.cue.nucleo.nuclearProyect.infrastructure.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-@NoArgsConstructor
+@RequestMapping("/admin")
 public class AdminController {
-    @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "prueba";
+    @GetMapping("/prove")
+    public String prove(){
+        return "prove admin";
     }
 }
