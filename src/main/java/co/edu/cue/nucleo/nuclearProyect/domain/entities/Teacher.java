e4738dae3ba4fcb4537bf97312764f8682673198
Andres <atoro1025@cue.edu.co>
2023-06-03 18:01:43 -0500

    feat/task-course-schedule

--- src/main/java/co/edu/cue/nucleo/nuclearProyect/domain/entities/Teacher.java
+++ src/main/java/co/edu/cue/nucleo/nuclearProyect/domain/entities/Teacher.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Data
 @Entity
 @NoArgsConstructor
-public class Teacher{
+public class Teacher{ //hola esto tiene un error en las demas ramas
     @Id
     @Column (name = "id")
     private String id;
