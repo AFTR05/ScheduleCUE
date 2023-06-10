3774909c7b3690796a560674d5f129089ea9195e
Andres <atoro1025@cue.edu.co>
2023-06-10 13:08:08 -0500

    show table user

--- src/main/resources/static/js/adminPage.js
+++ src/main/resources/static/js/adminPage.js
@@ -1,9 +1,26 @@
-
-
+/*async function cargarStudents() {
+    const request = await fetch('student_ad/get-all', {
+        method: 'GET',
+        headers: getHeaders()
+    });
+    const students = await request.json();
+    console.log(students);
+    let listadoHtml = '';
+    for (let student of students){
+        let studentHtml = '<tr><td>'+student.name+'</td><td>' + student.email +'</td><td>'
+            + student.ownSemester.program.name+'</td><td>'+student.ownSemester.semester
+            + '</td><td>'+student.active+'</td></tr>';
+        console.log(studentHtml);
+        listadoHtml += studentHtml;
+    }
+    console.log(listadoHtml);
+    document.querySelector('#students-content').innerHTML = listadoHtml;
+}*/
 $(document).ready(function() {
     $('#admin-schedules').addClass('active');
     navigate('admin-schedules');
 });
+
 function navigate(page) {
     $('.nav-link').removeClass('active');
     $('#' + page).addClass('active');
@@ -13,6 +30,20 @@ function navigate(page) {
         method: 'GET',
         success: function(response) {
             $('#content').html(response);
+            switch (page) {
+                case 'admin-students':
+                     getStudents();
+                    break;
+                case 'admin-admins':
+                     getAdministrator();
+                    break;
+                case 'admin-teachers':
+                    getTeacher();
+                    break;
+            }
+/*            if (page === 'admin-students') {
+                cargarStudents();
+            }*/
         },
         error: function() {
             $('#content').html('Error al cargar la página.');
@@ -51,3 +82,73 @@ document.getElementById("admin-subjects").onclick = function() {
 document.getElementById("admin-teachers").onclick = function() {
     navigate('admin-teachers');
 };
+
+function getHeaders() {
+    return {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.token
+    };
+}
+
+async function getStudents() {
+    const request = await fetch('student_ad/get-all', {
+        method: 'GET',
+        headers: getHeaders()
+    });
+    const students = await request.json();
+    console.log(students);
+    let listadoHtml = '';
+    for (let student of students){
+        let studentHtml = '<tr><td>'+student.name+'</td><td>' + student.email +'</td><td>'
+            + student.ownSemester.program.name+'</td><td>'+student.ownSemester.semester
+            + '</td><td>'+student.active+'</td></tr>';
+        console.log(studentHtml);
+        listadoHtml += studentHtml;
+    }
+    console.log(listadoHtml);
+    document.querySelector('#students-content').innerHTML = listadoHtml;
+}
+
+async function getTeacher(){
+    const request = await fetch('teacher_ad/get-all', {
+        method: 'GET',
+        headers: getHeaders()
+    });
+    const teachers = await request.json();
+    console.log(teachers);
+    let listadoHtml = '';
+    for (let teacher of teachers){
+        let teacherHtml = '<tr><td>'+teacher.name+'</td><td>' + teacher.email + '</td><td>'+teacher.active+'</td></tr>';
+        console.log(teacherHtml);
+        listadoHtml += teacherHtml;
+    }
+    console.log(listadoHtml);
+    document.querySelector('#teachers-content').innerHTML = listadoHtml;
+}
+
+async function getAdministrator(){
+    const request = await fetch('admin_ad/get-all', {
+        method: 'GET',
+        headers: getHeaders()
+    });
+    const admins = await request.json();
+    console.log(admins);
+    let listadoHtml = '';
+    for (let admin of admins){
+        let adminHtml = '<tr><td>'+admin.name+'</td><td>' + admin.email +'</td><td>'
+            + admin.typeAdmin.typeAdmin+'</td><td>'+admin.active+'</td></tr>';
+        console.log(adminHtml);
+        listadoHtml += adminHtml;
+    }
+    console.log(listadoHtml);
+    document.querySelector('#admins-content').innerHTML = listadoHtml;
+}
+
+function getHeaders() {
+    return {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.token
+    };
+}
